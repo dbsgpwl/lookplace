6754d072c29ba3806eebbbfb791425215dc116df
yunhyeji <dbsgpwl97@gmail.com>
2022-09-14 22:51:33 +0900

    맵 컨트롤러 오타 수정

--- src/main/java/com/look/controller/MapController.java
+++ src/main/java/com/look/controller/MapController.java
@@ -11,46 +11,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.look.service.BoardService;
 
 @Controller					
-@RequestMapping("/trip/*") 
+@RequestMapping("/map/*") 
 public class MapController {
-	 
-		private static final Logger log = LoggerFactory.getLogger(MapController.class);
-		
-		
-		@GetMapping("/area")
-		public void areaGET(Model model) {
-			
-			log.info("지역 페이지 진입");			
-		}
-		
-		@GetMapping("/best")
-		public void wishlistGET(Model model) {
-			
-			log.info("인기 페이지 진입");		
-		}		
-		
-		@GetMapping("/course")
-		public void leavememberGET(Model model) {
-			
-			log.info("코스 페이지 진입");			
-		}
-		
-		@GetMapping("/entire")
-		public void entireGET(Model model) {
-			
-			log.info("전체 페이지 진입");			
-		}
-		
-		@GetMapping("/travel-p")
-		public void travelpGET(Model model) {
-			
-			log.info("여행지 상세 페이지 진입");			
-		}
-		
-		@GetMapping("/travel-c")
-		public void travelcGET(Model model) {
-			
-			log.info("코스 상세 페이지 진입");
-			
+
+		private static final Logger log = LoggerFactory.getLogger(TripController.class);
+
+
+		@GetMapping("/mapapi")
+		public void mapGET(Model model) {
+
+			log.info("지도 페이지 진입");			
 		}
 }
